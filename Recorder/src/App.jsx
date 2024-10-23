8410d3f605532132272bbe6c6d67b2af9c1287e3
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-10-23 23:39:03 +0530

    Add react-audio-voice-recorder library and * background

---
+++ Recorder/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Home from "./components/Home";
+
+function App() {
+	return (
+		<div className="h-screen w-screen flex justify-center items-center">
+			<Home />
+		</div>
+	);
+}
+
+export default App;
---
+++ Recorder/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css"; 
+function Home() {
+	return (
+		<div className="star-background  outer-border flex justify-center items-center ">
+			<div className="inner-border w-[1152px] h-[560px]  flex justify-center items-center">
+				<div></div>
+			</div>
+		</div>
+	);
+}
+
+// box-shadow: 0px 4px 70px 10px #FFFFFF0D;
+
+export default Home;
